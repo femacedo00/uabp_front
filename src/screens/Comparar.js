558590e8b9eb91552aa6b010b0b7fdb6973100fb
Felipe <felipe.macedo00@gmail.com>
2024-06-18 19:55:13 -0300

    comparador funcional

--- src/screens/Comparar.js
+++ src/screens/Comparar.js
@@ -1,69 +1,95 @@
 import { ListGroup, Row, Col } from 'react-bootstrap';
 import React, { useState, useContext } from 'react';
 import { CartContext } from '../context/CartContext';
+import FetchProducts from '../FetchProducts';
 import ListType from './ListType.js';
 import '../styles/Comparar.css';
 
 export default function Comparar() {
     const [typeFilter, setTypeFilter] = useState("Mercado");
     const { itemsList } = useContext(CartContext);
-
+    const { data: mercados, loading, error } = FetchProducts(localStorage.getItem('token'), "http://localhost:8080/v1/mercado/", undefined);
     console.log(itemsList);
 
-    const returnAPI = {
-        "Mercado": [
-            {
-                "nome": "Supermercado da Maria",
-                "rua": "Avenida das Palmeiras, 1332",
-                "cnpj": "13458-789",
-                "valor": 1724.32
-            },
-            {
-                "nome": "Mercadinho da Ana",
-                "rua": "Avenida das Palmeiras, 1332",
-                "cnpj": "13458-789",
-                "valor": 2823.89
-            }
-        ],
-        "Essenciais": [
-            {
-                "nome": "Geleia de morango",
-                "nomeMercado": "Mercadinho da Ana",
-                "marca": "Doces da Vovó",
-                "uni_grand": "g",
-                "num_grand": 400,
-                "valor": 28
-            },
-            {
-                "nome": "Geleia de morango",
-                "nomeMercado": "Mercadinho da Ana",
-                "marca": "Doces da Vovó",
-                "uni_grand": "g",
-                "num_grand": 400,
-                "valor": 28
-            }
-        ],
-        "Itens": [
-            {
-                "nome": "Geleia de morango",
-                "nomeMercado": "Mercadinho da Ana",
-                "marca": "Doces da Vovó",
-                "uni_grand": "g",
-                "num_grand": 1000,
-                "valor": 45
-            },
-            {
-                "nome": "Geleia de morango",
-                "nomeMercado": "Mercadinho da Ana",
-                "marca": "Doces da Vovó",
-                "uni_grand": "g",
-                "num_grand": 400,
-                "valor": 75
-            }
-        ]
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error: {error.message}</p>;
+
+    let resultValue = {
+        "Mercado": [],
+        "Essenciais": [],
+        "Itens": []
     };
 
-    const filteredData = returnAPI[typeFilter] || [];
+    let nomeAux = "";
+    let pos = 0;
+    let valorMercadoItem = 0;
+
+    mercados.map((mercado) => {
+        if (nomeAux !== mercado.nome) {
+            pos = resultValue["Mercado"].length;
+            resultValue["Mercado"][pos] = {
+                "nome": mercado.nome,
+                "rua": mercado.rua,
+                "cep": mercado.cep,
+                "valor": 0
+            };
+            nomeAux = mercado.nome;
+        }
+
+        itemsList.essenciais.map((essencial) => {
+            let posEssencial = resultValue["Essenciais"].length;
+            let valorMinItem = null;
+
+            mercado.precoMercado.map((preco) => {
+                if (preco.item.upc_a === essencial.upc_a) {
+                    valorMercadoItem = preco.preco * essencial.quantity;
+
+                    if (valorMinItem === null || valorMinItem > valorMercadoItem) valorMinItem = valorMercadoItem;
+                    resultValue["Mercado"][pos].valor += valorMercadoItem;
+                }
+            });
+
+            if(valorMinItem){
+                resultValue["Essenciais"][posEssencial] = {
+                    "nomeMercado": mercado.nome,
+                    "nome": essencial.nome,
+                    "marca": essencial.marca,
+                    "uni_grand": essencial.uni_grand,
+                    "num_grand": essencial.num_grand,
+                    "valor": valorMinItem
+                };
+            }
+        });
+
+        itemsList.comuns.map((comum) => {
+            let posComum = resultValue["Itens"].length;
+            let valorMinItem = null;
+
+            mercado.precoMercado.map((preco) => {
+                if (preco.item.upc_a === comum.upc_a) {
+                    valorMercadoItem = preco.preco * comum.quantity;
+
+                    if (valorMinItem === null || valorMinItem > valorMercadoItem) valorMinItem = valorMercadoItem;
+                    resultValue["Mercado"][pos].valor += valorMercadoItem;
+                }
+            });
+
+            if(valorMinItem){
+                resultValue["Itens"][posComum] = {
+                    "nomeMercado": mercado.nome,
+                    "nome": comum.nome,
+                    "marca": comum.marca,
+                    "uni_grand": comum.uni_grand,
+                    "num_grand": comum.num_grand,
+                    "valor": valorMinItem
+                };
+            }
+        });
+    });
+
+    resultValue["Mercado"] = resultValue["Mercado"].filter(m => m.valor > 0).sort((a, b) => a.valor - b.valor);
+
+    const filteredData = resultValue[typeFilter] || [];
 
     return (
         <div className="Comparar">
